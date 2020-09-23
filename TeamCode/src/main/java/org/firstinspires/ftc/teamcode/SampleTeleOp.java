c2a21d36c4d71e7b874a8f161455cd62a7b13e7d
cameron <cameron.italianer@gmail.com>
2020-09-23 16:49:20 -0500

    Matthew Cremer is a big dumb idiot. He does not play the french horn like a chad. Play the french horn so you can win
    Thank you joey

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTeleOp.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "tele op", group = "yeah")
 public class SampleTeleOp extends LinearOpMode {
@@ -12,6 +14,25 @@ public class SampleTeleOp extends LinearOpMode {
         telemetry.addData("", "yello, world");
         telemetry.update();
 
-        while (opModeIsActive());
+        DcMotor lf = hardwareMap.dcMotor.get("lf");
+        DcMotor rf = hardwareMap.dcMotor.get("lb");
+        DcMotor lb = hardwareMap.dcMotor.get("rf");
+        DcMotor rb = hardwareMap.dcMotor.get("rb");
+
+        lf.setDirection(DcMotor.Direction.REVERSE);
+        rf.setDirection(DcMotor.Direction.REVERSE);
+        lb.setDirection(DcMotor.Direction.FORWARD);
+        rb.setDirection(DcMotor.Direction.FORWARD);
+
+        while (opModeIsActive()) {
+            lf.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
+            rf.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x);
+            lb.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x);
+            rb.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
+
+            telemetry.update();
+        }
+
+        lf.setPower(0);
     }
 }
