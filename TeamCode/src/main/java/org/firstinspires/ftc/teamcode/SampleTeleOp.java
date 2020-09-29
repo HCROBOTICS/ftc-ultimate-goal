8e029d0ddf8f9596befb89f0771ecec09ea817c8
verbicide <9778314+verbicide@users.noreply.github.com>
2020-09-29 16:47:43 -0500

    some sort of autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTeleOp.java
@@ -7,6 +7,11 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "tele op", group = "yeah")
 public class SampleTeleOp extends LinearOpMode {
+    DcMotor lf;
+    DcMotor rf;
+    DcMotor lb;
+    DcMotor rb;
+
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
@@ -14,25 +19,40 @@ public class SampleTeleOp extends LinearOpMode {
         telemetry.addData("", "yello, world");
         telemetry.update();
 
-        DcMotor lf = hardwareMap.dcMotor.get("lf");
-        DcMotor rf = hardwareMap.dcMotor.get("lb");
-        DcMotor lb = hardwareMap.dcMotor.get("rf");
-        DcMotor rb = hardwareMap.dcMotor.get("rb");
+        /* TODO: fix these wrong lines */
+        lf = hardwareMap.dcMotor.get("lf");
+        rf = hardwareMap.dcMotor.get("lb");
+        lb = hardwareMap.dcMotor.get("rf");
+        rb = hardwareMap.dcMotor.get("rb");
 
         lf.setDirection(DcMotor.Direction.REVERSE);
         rf.setDirection(DcMotor.Direction.REVERSE);
         lb.setDirection(DcMotor.Direction.FORWARD);
         rb.setDirection(DcMotor.Direction.FORWARD);
 
-        while (opModeIsActive()) {
-            lf.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
+        {
+            /* lf.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
             rf.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x);
             lb.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x);
             rb.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
+            */
+
+            forwards(1);
+            sleep(1000);
+            forwards(0);
+            sleep(1000);
+            forwards(-1);
+            sleep(1000);
+            forwards(0);
 
             telemetry.update();
         }
+    }
 
-        lf.setPower(0);
+    void forwards(float power) {
+        lf.setPower(power);
+        rf.setPower(power);
+        lb.setPower(power);
+        rb.setPower(power);
     }
 }
