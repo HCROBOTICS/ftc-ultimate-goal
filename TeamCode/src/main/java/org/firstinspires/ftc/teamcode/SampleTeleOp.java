3fe235e97cc172c8cbfd6c59e2b3ca08354b4411
cameron <cameron.italianer@gmail.com>
2020-11-10 15:59:17 -0600

    encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTeleOp.java
@@ -13,6 +13,11 @@ public class SampleTeleOp extends LinearOpMode {
     DcMotor lb;
     DcMotor rb;
 
+    int encodersFront,
+        encodersBack,
+        encodersLeft,
+        encodersRight;
+
     DcMotor llaunch;
     DcMotor rlaunch;
     DcMotor pull;
@@ -28,16 +33,18 @@ public class SampleTeleOp extends LinearOpMode {
         telemetry.addData("", " The Code does not work");
         telemetry.update();
 
-
         lf = hardwareMap.dcMotor.get("lf");
         rf = hardwareMap.dcMotor.get("rf");
         lb = hardwareMap.dcMotor.get("lb");
         rb = hardwareMap.dcMotor.get("rb");
-
         lf.setDirection(DcMotor.Direction.REVERSE);
         rf.setDirection(DcMotor.Direction.FORWARD);
         lb.setDirection(DcMotor.Direction.REVERSE);
         rb.setDirection(DcMotor.Direction.FORWARD);
+        lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // For the Launcher
 
@@ -59,23 +66,29 @@ public class SampleTeleOp extends LinearOpMode {
         */
 
         while (opModeIsActive()) {
-
+            encodersFront = (lf.getCurrentPosition() + rf.getCurrentPosition()) / 2;
+            encodersBack  = (lb.getCurrentPosition() + rb.getCurrentPosition()) / 2;
+            encodersLeft  = (lf.getCurrentPosition() + lb.getCurrentPosition()) / 2;
+            encodersRight = (rf.getCurrentPosition() + rb.getCurrentPosition()) / 2;
+            if (gamepad1.a) {
+                resetEncoders();
+                telemetry.addData("encoders", "reset");
+            } else {
+                telemetry.addData("front", encodersFront);
+                telemetry.addData("back", encodersBack);
+                telemetry.addData("left", encodersLeft);
+                telemetry.addData("right", encodersRight);
+            }
 
             lf.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x);
             rf.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x);
             lb.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x);
             rb.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
 
-
             llaunch.setPower(gamepad1.right_trigger);
             rlaunch.setPower(gamepad1.right_trigger);
             pull.setPower(gamepad1.left_trigger);
-
-
-
         }
-
-
     }
 
     void forwards(float power) {
@@ -86,23 +99,29 @@ public class SampleTeleOp extends LinearOpMode {
     }
 
     void righturn(float power) {
-
         lf.setPower(power);
         rf.setPower(-power);
         lb.setPower(power);
         rb.setPower(-power);
-
     }
 
     void lefturn(float power) {
-
         lf.setPower(-power);
         rf.setPower(power);
         lb.setPower(-power);
         rb.setPower(power);
-
     }
 
+    void resetEncoders() {
+        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
 }
 
